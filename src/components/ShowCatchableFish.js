620537f662c66395b0ff11827257be60a120e5c9
Carly Burkhart <21320169+TimeIsOnMySide@users.noreply.github.com>
2023-08-10 18:56:42 -0700

    API is down. Made notes for what to work on next.

--- src/components/ShowCatchableFish.js
+++ src/components/ShowCatchableFish.js
@@ -2,6 +2,9 @@ import { useDispatch, useSelector } from 'react-redux'
 import { getAllFish } from '../features/fish/fishSlice'
 import { useEffect } from 'react'
 
+// Notes for what to work on when API starts working again:
+//
+
 const ShowCatchableFish = () => {
     const { allFish, isLoading } = useSelector((state) => state.fish)
 
@@ -20,10 +23,10 @@ const ShowCatchableFish = () => {
     }
 
     //
-    //Find catchable fish logic
+    // Find catchable fish logic
     //
     const catchableFish = [] // This is where ill push the catchable fish after evaluating the current time
-    let fishNameArr = [] //These are all the first level object keys of the objects I need to check
+    let fishNameArr = [] // These are all the first level object keys of the objects I need to check
     for (const fish in allFish) {
         fishNameArr.push(fish)
     }
@@ -48,9 +51,9 @@ const ShowCatchableFish = () => {
         currentHour = date.getHours() + 1
     }
 
-    //Loop through the array of fish names and search for the info of that nested object
+    // Loop through the array of fish names and search for the info of that nested object
     for (let i = 0; i < fishNameArr.length; i++) {
-        let fishName = fishNameArr[i] //Set fish Name
+        let fishName = fishNameArr[i] // Set fish Name
         let fishObj = allFish[fishName] // Fish object w/ all info
         let availableMonths = fishObj.availability['month-array-northern'] // Set available months of the fish
         let availebleTimes = fishObj.availability['time-array']
@@ -80,12 +83,16 @@ const ShowCatchableFish = () => {
     }
 
     const renderFish = () => {
-        catchableFish.map((fish) => {
-            console.log(allFish[fish].icon_uri)
+        return catchableFish.map((fish) => {
+            // console.log(allFish[fish].icon_uri)
             return (
                 <div>
                     <p>{fish}</p>
-                    <img src={allFish[fish].icon_uri} alt="fish icon" />
+                    <img
+                        // eslint-disable-next-line react/style-prop-object
+                        src={allFish[fish].icon_uri}
+                        alt="fish icon"
+                    />
                 </div>
             )
         })
